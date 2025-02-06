c136197d8717fa85838c3470dfd90c8b7f4398fc
Dravernuss <esteban16.rodas@gmail.com>
2025-02-05 20:48:22 -0500

    Finishing migration to ContextAPI

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,28 +1,9 @@
-import { Activity } from "../types";
-import { categories } from "../data/categories";
-import { Dispatch, useMemo } from "react";
 import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
-import { ActivityActions } from "../reducers/activity-reducer";
+import { useActivity } from "../hooks/useActivity";
 
-type ActivityListProps = {
-  activities: Activity[];
-  dispatch: Dispatch<ActivityActions>;
-};
+export default function ActivityList() {
+  const { state, dispatch, isEmptyActivities, categoryName } = useActivity();
 
-export default function ActivityList({
-  activities,
-  dispatch,
-}: ActivityListProps) {
-  const categoryName = useMemo(
-    () => (category: Activity["category"]) =>
-      categories.map((cat) => (cat.id === category ? cat.name : "")),
-    [activities]
-  );
-
-  const isEmptyActivities = useMemo(
-    () => activities.length === 0,
-    [activities]
-  );
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
@@ -31,7 +12,7 @@ export default function ActivityList({
       {isEmptyActivities ? (
         <p className="text-center my-5">No hay actividades aún...</p>
       ) : (
-        activities.map((activity) => (
+        state.activities.map((activity) => (
           <div
             key={activity.id}
             className="px-5 py-10 bg-white mt-5 flex justify-between"
--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,10 +1,12 @@
-import { Dispatch, ReactNode, createContext, useReducer } from "react";
+import { Dispatch, ReactNode, createContext, useMemo, useReducer } from "react";
 import {
   ActivityActions,
   activityReducer,
   ActivityState,
   initialState,
 } from "../reducers/activity-reducer";
+import { Activity } from "../types";
+import { categories } from "../data/categories";
 
 type ActivityProviderProps = {
   children: ReactNode;
@@ -13,6 +15,11 @@ type ActivityProviderProps = {
 type ActivityContextProps = {
   state: ActivityState;
   dispatch: Dispatch<ActivityActions>;
+  caloriesConsumed: number;
+  caloriesBurned: number;
+  netCalories: number;
+  categoryName: (category: Activity["category"]) => string[];
+  isEmptyActivities: boolean;
 };
 
 export const ActivityContext = createContext<ActivityContextProps>(null!);
@@ -20,8 +27,54 @@ export const ActivityContext = createContext<ActivityContextProps>(null!);
 export const ActivityProvider = ({ children }: ActivityProviderProps) => {
   const [state, dispatch] = useReducer(activityReducer, initialState);
 
+  const caloriesConsumed = useMemo(
+    () =>
+      state.activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [state.activities]
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      state.activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+    [state.activities]
+  );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [state.activities]
+  );
+
+  const categoryName = useMemo(
+    () => (category: Activity["category"]) =>
+      categories.map((cat) => (cat.id === category ? cat.name : "")),
+    [state.activities]
+  );
+
+  const isEmptyActivities = useMemo(
+    () => state.activities.length === 0,
+    [state.activities]
+  );
+
   return (
-    <ActivityContext.Provider value={{ state, dispatch }}>
+    <ActivityContext.Provider
+      value={{
+        state,
+        dispatch,
+        caloriesConsumed,
+        caloriesBurned,
+        netCalories,
+        categoryName,
+        isEmptyActivities,
+      }}
+    >
       {children}
     </ActivityContext.Provider>
   );
